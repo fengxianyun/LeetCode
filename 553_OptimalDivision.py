d32264476182edb9fe2537c9a8c337903ecd697a
DESKTOP-58H7SAQ\raytine <937002565@qq.com>
2017-10-31 00:06:03 +0800

    553

---
+++ 553_OptimalDivision.py
@@ -0,0 +1,39 @@
+# coding: utf-8
+'''
+Given a list of positive integers, the adjacent integers will perform the float division. For example, [2,3,4] -> 2 / 3 / 4.
+
+However, you can add any number of parenthesis at any position to change the priority of operations. You should find out how to add parenthesis to get the maximum result, and return the corresponding expression in string format. Your expression should NOT contain redundant parenthesis.
+
+Example:
+Input: [1000,100,10,2]
+Output: "1000/(100/10/2)"
+Explanation:
+1000/(100/10/2) = 1000/((100/10)/2) = 200
+However, the bold parenthesis in "1000/((100/10)/2)" are redundant, 
+since they don't influence the operation priority. So you should return "1000/(100/10/2)". 
+
+Other cases:
+1000/(100/10)/2 = 50
+1000/(100/(10/2)) = 50
+1000/100/10/2 = 0.5
+1000/100/(10/2) = 2
+
+题目要求给定一组数据（正整数），每个数字间是除法关系，但是可以随意加括号。求一个最大的算式。
+第一个数肯定是被除数，只要后面的数越小，结果就越大，所以只要把后面的都括起来就好。
+93.83%
+'''
+
+
+class Solution(object):
+    def optimalDivision(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: str
+        """
+        if len(nums) == 1:
+            return str(nums[0])
+        if len(nums) == 2:
+            return '{}/{}'.format(nums[0], nums[1])
+        other = '/'.join([str(i) for i in nums[1:]])
+        return '{}/({})'.format(nums[0], other)
+        
